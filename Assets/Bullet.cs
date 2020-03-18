9dc4a3db8104c886a5121afcb34de4cb5f8f465a
suptoasty <jason.lonsinger@gmail.com>
2020-03-18 18:56:08 -0500

    Added Bullet lifeTime

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,10 +6,12 @@ public class Bullet : MonoBehaviour
 {
     public int damage = 1;
     public float bulletSpeed = 5.0f;
+    public float lifeTime = 5.0f;
 
     public void OnFired(float direction) {
         GetComponent<SpriteRenderer>().flipX = direction < 0 ? true : false;
         GetComponent<Rigidbody2D>().AddForce(new Vector2(direction*bulletSpeed, 0.0f), ForceMode2D.Impulse);
+        Destroy(gameObject, lifeTime);
     }
 
     public void OnTriggerEnter2D(Collider2D collider) {
